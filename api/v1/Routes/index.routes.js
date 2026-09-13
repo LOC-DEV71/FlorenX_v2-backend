2119b1a4461d94a526f02fe41ef219941bc75ac2
LOC-DEV71 <lamchilocwork5@gmail.com>
2026-09-13 15:02:25 +0700

    api running

--- api/v1/Routes/index.routes.js
+++ api/v1/Routes/index.routes.js
@@ -1,7 +1,8 @@
 const adminRoutes = require("./Admin/admin.routes");
 const clientRoutes = require("./Client/client.routes");
-
+const ApiRunning = require("./ApiRunning");
 module.exports = (app) => {
+  app.use("/", ApiRunning);
   app.use("/api/v1/admin", adminRoutes);
   app.use("/api/v1/client", clientRoutes);
 };
